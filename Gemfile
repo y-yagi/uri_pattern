6467da75663b690a818850defd4fa2a95fb33daa
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2026-06-14 07:28:32 +0900

    First commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in uri_pattern.gemspec
+gemspec
+
+gem "irb"
+gem "test-unit"
+gem "rake"
---
+++ test/test_uri_pattern.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestURIPattern < Test::Unit::TestCase
+  def test_that_it_has_a_version_number
+    refute_nil ::URIPattern::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
